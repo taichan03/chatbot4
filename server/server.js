1f1c428881b3b2a6e36484a747ecc8f4658b818b
taichan03 <tai.c.chan3@gmail.com>
2023-05-20 14:40:23 -0400

    changes to chatbot api

--- server/server.js
+++ server/server.js
@@ -25,18 +25,23 @@ app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    const messages = prompt.map((message, index) => {
+      if (index % 2 === 0) {
+        // If the index is even, set the role as "system"
+        return { role: "system", content: message.content };
+      } else {
+        // If the index is odd, set the role as "user"
+        return { role: "user", content: message.content };
+      }
+    });
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text,
+      bot: response.data.choices[0].message,
     });
   } catch (error) {
     console.error(error);
